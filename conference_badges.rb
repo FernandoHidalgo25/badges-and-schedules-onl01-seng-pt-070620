3ca1ec160276950b766f99401926464ab9735a2c
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-14 02:04:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,12 @@ rooms = [1,2,3,4,5,6,7]
 def assign_rooms(speakers)
   speakers.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
   end
+  
+def printer(speakers)
+ batch_badge_creator(speakers).each do |badge|
+  puts badge
+ end
+ assign_rooms(speakers).each do |assignment|
+  puts assignment
+ end
+end
